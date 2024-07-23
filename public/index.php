3841d524d31b89f28cb06e9a3b245566187852b8
Usman Arif <usmanarif.9219@gmail.com>
2024-07-24 00:29:28 +0500

    change path

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 
-die("working");
-
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
@@ -13,6 +11,8 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 
 // Register the Composer autoloader...
 require __DIR__ . '/../vendor/autoload.php';
+
+die("now vendor added");
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__ . '/../bootstrap/app.php')
