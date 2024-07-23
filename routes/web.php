4dcb58f558ead9b448fbd0500821f07e77123526
Usman Arif <usmanarif.9219@gmail.com>
2024-07-24 00:32:45 +0500

    remove index file

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return redirect()->route('student.login');
 });
+
+Route::get('/optimize', function () {
+    Artisan::call('optimize:clear');
+    dd("cleared");
+});
+
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
 require __DIR__ . '/student.php';
