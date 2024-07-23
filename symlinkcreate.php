9bca53cf9b682025e01236a71fa788af5374df48
Usman Arif <usmanarif.9219@gmail.com>
2024-07-24 01:13:09 +0500

    new way

--- symlinkcreate.php
+++ symlinkcreate.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "work";
+
 $targetFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage/app/public';
 $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/public/storage';
 symlink($targetFolder, $linkFolder);
