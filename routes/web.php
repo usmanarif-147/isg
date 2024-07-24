a7e46c41bbbd6a7f84010f8bc0df551fd44a3d2b
Usman Arif <usmanarif.9219@gmail.com>
2024-07-24 13:12:26 +0500

    remove file

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::get('/optimize', function () {
     Artisan::call('optimize:clear');
-    dd("cleared");
+    dd("cache cleared");
 });
 
 Route::get('/storage-link-artisan', function () {
